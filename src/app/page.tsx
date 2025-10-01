16b10bab7df93dfb2e06ce1c1b6a3ae5fb73924d
roaziy <erkhemtur389@gmail.com>
2025-10-01 17:17:59 +0800

    fixed the text xD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen bg-zinc-900">
       <div className="flex justify-center items-center h-full">
-        <p className="text-white font-bold md:text-xl text-lg text-center">My little busy now, so I will update my portfolio soon... 😝</p>
+        <p className="text-white font-bold md:text-xl text-lg text-center">I'm little busy now, so I will update my portfolio soon... 😝</p>
       </div>
     </div>
   );
